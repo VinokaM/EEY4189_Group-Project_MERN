169aa0f0cd48c652548bf80c080f4d02e4db2266
Vinoka Malith <90492658+VinokaM@users.noreply.github.com>
2024-06-05 15:39:09 +0530

    This is initial commit
    create front end package and back end side package. and connect to the database

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./DB_Connection");
+const userRoutes = require("./routes/sampleRoutes")
+
+app.use(express.json());
+app.use(cors());
+
+// database connection
+connection();
+
+app.use("/api/users", userRoutes);
+
+
+const port = process.env.PORT || 6000;
+app.listen(port, console.log(`Listening on port ${port}...`));
