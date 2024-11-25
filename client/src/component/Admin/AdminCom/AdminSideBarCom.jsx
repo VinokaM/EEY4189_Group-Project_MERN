3042fafb59a2af1c4da8c199568f8b117ec0daa7
WimansaW <s92065746@ousl.lk>
2024-11-25 19:51:55 +0530

    created Admin Top Bar and Side bar  with CSS
    I have created admin folder to create every component of Admin and also have done Admin Top Bar and Admin Side Bar with CSS.

---
+++ client/src/component/Admin/AdminCom/AdminSideBarCom.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { SidebarData } from '../AdminSideBarData';
+import { NavLink } from 'react-router-dom';
+import './sidebar.css'; 
+
+const name = localStorage.getItem('firstName');
+const email = localStorage.getItem('email');
+
+const Sidebar = () => {
+  return (
+    <React.Fragment>
+      <section>
+        <div class="sidebar">
+          <div className="sidebar-header">
+            <h2>{name}</h2>
+            <h3>{email}</h3>
+          </div>
+          {SidebarData.map((item, index) => {
+            return (
+              <div key={index} class="title">
+                <NavLink
+                  to={item.path}
+                  className={({ isActive }) =>
+                    isActive ? 'active-link' : 'normal-link'
+                  }
+                >
+                  <span class="title">{item.title}</span>
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Sidebar;
