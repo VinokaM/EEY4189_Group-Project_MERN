9e51f8b50e4de675862c415bdacbcb8efc6d3dbf
Neranjitha <s92068614@ousl.lk>
2024-12-07 19:51:25 +0530

    Created Doctor navigation title and path
    I have created navigation path for doctors function in side navigation bar. I added sample path (/sample), after creating doctors function I will change that path.

---
+++ client/src/component/Doctor/DoctorSideBarData.jsx
@@ -0,0 +1,43 @@
+
+
+export const SidebarData=[
+    {
+        title: "Sample",
+        path: "/*",
+      },
+      {
+        title: "Appointments",
+        path: "/appointments",
+      },
+      {
+        title: "Update Your Details",
+        path: "/doctorprofile",
+      },
+      {
+        title: "Chat",
+        path: "/chat",
+      },
+      {
+        title: "Sample",
+        path: "/sample5",
+      },
+     
+      {
+        title: "Sample",
+        path: "/sample6",
+      },
+      {
+        title: "Sample",
+        path: "/sample7",
+      },
+      {
+        title: "Delete Users",
+        path: "/deleteuser",
+        
+      },
+      {
+        title: "Edit Profile",
+        path: "/updateadmin",
+       
+      },
+]
