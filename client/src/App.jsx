91e98c402e0fe537b85aa42cbd766f1fe4c75dbc
Vinoka Malith <S92068703@ousl.lk>
2024-12-19 22:03:01 +0530

    Update App.jsx
    Add role base login path and link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,34 @@
+<<<<<<< Updated upstream
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+=======
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Signup from "./component/SignUp";
+import Login from './component/LogIn';
+import AdminMain from './component/Admin/AdminMain';
+import DoctorMain from './component/Doctor/DoctorMain';
+import PatientMain from "./component/Patient/HomeScreen";
+import StaffMain from "./component/Staff/StaffMain";
+
+let user;
+>>>>>>> Stashed changes
 
 function App() {
   const [count, setCount] = useState(0)
 
+  try {
+    user = JSON.parse(localStorage.getItem("role"));
+  } catch (error) {
+    console.error("Error parsing user from localStorage:", error);
+    
+    return <Navigate to="/login" />;
+  }
+
   return (
+<<<<<<< Updated upstream
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -33,3 +55,26 @@ function App() {
 }
 
 export default App
+=======
+    <Router>
+      <Routes>
+        
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<PatientMain/>} />
+        <Route path="/signup" element={<Signup />} />
+
+        {user && user.role === "admin" && <Route path="/*" element={<AdminMain />} />}
+        {user && user.role === "doctor" && <Route path="/*" element={<DoctorMain />} />}
+        {user && user.role === "patient" && <Route path="/*" element={<PatientMain />} />}
+        {user && user.role === "staff" && <Route path="/*" element={<StaffMain />} />}
+        
+            
+      </Routes>
+      
+    </Router>
+  );
+}
+
+export default App;
+
+>>>>>>> Stashed changes
