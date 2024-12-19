34f9205159b427162192ddab040968b77481458c
Vinoka Malith <S92068703@ousl.lk>
2024-12-19 22:07:43 +0530

    Update App.jsx
    Add role base login path and link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-=======
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Signup from "./component/SignUp";
@@ -14,7 +8,6 @@ import PatientMain from "./component/Patient/HomeScreen";
 import StaffMain from "./component/Staff/StaffMain";
 
 let user;
->>>>>>> Stashed changes
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,34 +21,6 @@ function App() {
   }
 
   return (
-<<<<<<< Updated upstream
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-=======
     <Router>
       <Routes>
         
@@ -77,4 +42,3 @@ export default App
 
 export default App;
 
->>>>>>> Stashed changes
