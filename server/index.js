8ecdc34274eafe9804891337e5deae0d759f13fc
Vinoka Malith <S92068703@ousl.lk>
2024-12-20 22:36:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const userManage = require("./routes/userManage");
+const doctorRoutes = require("./routes/doctorRoutes");
+const doctorDetailsManage = require("./routes/doctorDetailsManage");
 const connection = require("./DB_Connection");
 const userRoutes = require("./routes/signUpTask");
 const login = require("./routes/loginTask");
+const messageRoutes = require("./routes/messageRoutes");
 
 app.use(express.json());
 app.use(cors());
@@ -14,6 +18,10 @@ connection();
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth",login);
+app.use(userManage);
+app.use(doctorRoutes);
+app.use("/doctor",doctorDetailsManage);
+app.use(messageRoutes);
 
 
 const port = process.env.PORT || 8080;
